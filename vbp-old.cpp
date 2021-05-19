a22e674adc864a22c8a00c52f2b3cc0e45453669
ZHT <hoz043@ucsd.edu>
2021-05-18 20:01:25 -0700

    ...

--- vbp-old.cpp
+++ vbp-old.cpp
@@ -5,9 +5,9 @@ int main(int argc, char *argv[]) {
     
 
     std::string line;
-    std::getline(std::cin, line);
-
-    std::cout << line << std::endl;
-
+    while(true) {
+        std::getline(std::cin, line);
+        std::cout << line << std::endl;
+    }
     return 0;
 }
